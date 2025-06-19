aea95545de70b518114a7ae0a08ea88f8b20c7ec
PranavKhodhot <pranavkhodhot@gmail.com>
2025-06-19 15:56:46 -0400

    Fixed Handling of Order Submissions

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { ProductComponent } from './pages/product/product.component';
 import { BrowseComponent } from './pages/browse/browse.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { OrderDetailsComponent } from './pages/order-details/order-details.component';
 
 
 
@@ -11,7 +13,8 @@ export const routes: Routes = [
     { path: 'product/:id', component: ProductComponent },
     { path: 'checkout', component: CheckoutComponent },
     { path: 'browse', component: BrowseComponent },
-    { path: 'orders', component: BrowseComponent }
+    { path: 'orders', component: OrdersComponent },
+    { path: 'orders/:id', component: OrderDetailsComponent },
 
 
 ];
